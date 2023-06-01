f83d82f096cf578d00a80f7dc3fad824a2671a2e
filipi <filipichip7@gmail.com>
2023-06-01 17:09:21 -0300

    django model instance as api response studies

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,20 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": 'Hello World!'})
+    print(request.GET) # url query params
+    print(request.POST)
+    
+    body = request.body
+    data = {}
+    
+    try:
+        data = json.loads(body) # String of JSON data -> Python Dictionary
+    except:
+        pass
+    
+    # print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,9 +4,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={'abc': 123}, json={"query": "Hello World"}) # HTTP Request
+get_response = requests.get(endpoint, json={"query": "Hello World"}) # HTTP Request
 
 # print(get_response.text)
-print(get_response.json()) # Print raw text response
 # print(get_response.text)
 # print(get_response.status_code)
+print(get_response.json()) # Print raw text response
