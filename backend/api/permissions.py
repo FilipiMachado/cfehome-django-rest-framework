017c35a01e1d85290ad5433911f76cd214c91dbf
filipi <filipisee@hotmail.com>
2023-07-05 11:24:00 -0300

    workin with drf mixins

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -11,27 +11,3 @@ class IsStaffEditorPermission(permissions.DjangoModelPermissions):
         "PATCH": ["%(app_label)s.change_%(model_name)s"],
         "DELETE": ["%(app_label)s.delete_%(model_name)s"],
     }
-
-    """ def has_permission(self, request, view):
-        user = request.user
-        if not user.is_staff:
-            return False
-        return super().has_permission(request, view) """
-
-    """ def has_permission(self, request, view):
-        user = request.user
-        print(user.get_all_permissions())
-        if user.is_staff:
-            if user.has_perm("products.add_product"):  # Ex: "app_name.verb_model_name"
-                return True
-            if user.has_perm("products.delete_product"):
-                return True
-            if user.has_perm("products.change_product"):
-                return True
-            if user.has_perm("products.view_product"):
-                return True
-            return False
-        return False """
-
-    """ def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user """
