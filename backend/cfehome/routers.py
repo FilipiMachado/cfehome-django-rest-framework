ef9d1b1fe16353171b7deb3ea9c8232b9e65a3e9
filipi <filipisee@hotmail.com>
2023-07-05 11:31:58 -0300

    working with drf viewsets

---
+++ backend/cfehome/routers.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+
+
+from products.viewsets import ProductViewSet
+
+router = DefaultRouter()
+router.register("products-abc", ProductViewSet, basename="products")
+print(router.urls)
+urlpatterns = router.urls
